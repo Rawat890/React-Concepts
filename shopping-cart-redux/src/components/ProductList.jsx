2784dea5a321954cfb2e8ae71b25133ec2e802e7
Rawat890 <rohanrawat7508@gmail.com>
2025-10-04 14:15:11 +0530

    chore: file updated

--- shopping-cart-redux/src/components/ProductList.jsx
+++ shopping-cart-redux/src/components/ProductList.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 function ProductList() {
   return (
-    <div>ProductList</div>
+    <>
+    <NavBar/>
+    <div className='product-list'>
+    <div className='product-card'>
+      <img src='image' alt='Image-title'/>
+      <h2>Product title</h2>
+      <p>Price: $400</p>
+      <button>Add to cart</button>
+    </div>
+    </div>
+    </>
   )
 }
 
