54e5a5831c450db247182ad1c4884dab408bddbb
Rawat890 <rohanrawat7508@gmail.com>
2025-10-04 14:15:28 +0530

    chore: react-router-dom used

--- shopping-cart-redux/src/App.jsx
+++ shopping-cart-redux/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import NavBar from './components/NavBar';
 import ProductList from './components/ProductList';
-import Cart from './components/ProductList';
+import Cart from './components/Cart';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css'
 
 const App = () => {
   return (
-    <> 
-    <NavBar/>
-    <ProductList/>
-    <Cart/>
-    </>
+    <Router> 
+    <Routes>
+      <Route path='/' element={<ProductList/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+    </Routes>
+    </Router>
   )
 }
 
