054e1f9a4c81f76629de2ecc75dbfa65382e5f66
Rawat890 <rohanrawat7508@gmail.com>
2025-10-04 14:38:32 +0530

    chore: code formatted

--- shopping-cart-redux/src/components/ProductList.jsx
+++ shopping-cart-redux/src/components/ProductList.jsx
@@ -1,30 +1,36 @@
-import React, { useEffect } from 'react'
-import NavBar from './NavBar'
+import React, { useEffect, useState } from "react";
+import NavBar from "./NavBar";
 
 function ProductList() {
-  const fetchShopData = async()=>{
-    const response =  await fetch('https://dummyjson.com/products');
-    const data = await response.json();
-    console.log(data);
-  }
+  const [products, setProducts] = useState([]);
 
-  useEffect(()=>{
-   fetchShopData()
-  },[])
-  
+  const fetchShopData = async () => {
+    const response = await fetch("https://dummyjson.com/products");
+    const data = await response.json();
+    console.log(data.products);
+    setProducts(data.products);
+  };
+
+  useEffect(() => {
+    fetchShopData();
+  }, []);
+
   return (
     <>
-    <NavBar/>
-    <div className='product-list'>
-    <div className='product-card'>
-      <img src='image' alt='Image-title'/>
-      <h2>Product title</h2>
-      <p>Price: $400</p>
-      <button>Add to cart</button>
-    </div>
-    </div>
+      <NavBar />
+      <div className="product-list">
+        {products.map((product) => {
+          return(<div className="product-card" key={product.id}>
+              <img src={product.images} alt="Image-title" />
+              <h2>{product.title}</h2>
+              <p>Price: ${product.price}</p>
+              <button>Add to cart</button>
+            </div>
+          );
+        })}
+      </div>
     </>
-  )
+  );
 }
 
-export default ProductList
+export default ProductList;
