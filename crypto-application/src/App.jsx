d605774b5c0f110c34e1f33d2c35c06585464c97
Rawat890 <rohanrawat7508@gmail.com>
2025-10-05 16:14:14 +0530

    chore: routes added

--- crypto-application/src/App.jsx
+++ crypto-application/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from "react";
+import {
+  Navbar,
+  Exchanges,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+  HomePage,
+} from "./components/index";
+import "./App.css";
+import { Layout, Space, Switch, Typography } from "antd";
+import { Routes, Route, Link } from "react-router-dom";
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse <br />
+            All rights are reserved
+          </Typography.Title>
+          <Space>
+            <Link to={"/"}>Home</Link>
+            <Link to={"/exchanges"}>Exchanges</Link>
+            <Link to={"/news"}>News</Link>
+          </Space>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
