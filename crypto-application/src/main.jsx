de7b5b738fbb4d397976b60c63e317742d5563b6
Rawat890 <rohanrawat7508@gmail.com>
2025-10-06 14:31:45 +0530

    chore: store access provided to app

--- crypto-application/src/main.jsx
+++ crypto-application/src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
-     <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
-  </StrictMode>,
-)
+  </StrictMode>
+);
