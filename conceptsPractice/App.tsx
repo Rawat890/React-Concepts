95fa6545cbd0b1c6de7539eef6cfbc50edd13177
Rawat890 <rohanrawat7508@gmail.com>
2025-10-06 15:13:00 +0530

    chore: AppNavigator added

--- conceptsPractice/App.tsx
+++ conceptsPractice/App.tsx
@@ -1,11 +1,9 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import AppNavigator from './src/navigation/AppNavigator';
 
 function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <AppNavigator/>
   )
 }
 
