e0ffd2085e4011eb8e6e43c63695668d103d5148
Rawat890 <rohanrawat7508@gmail.com>
2025-10-06 14:31:57 +0530

    chore: Home page updated

--- crypto-application/src/components/HomePage.jsx
+++ crypto-application/src/components/HomePage.jsx
@@ -1,8 +1,20 @@
+import { Col, Row, Statistic, Typography } from 'antd'
 import React from 'react'
+
+const {Title}= Typography;
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+     <Title level={2} className='heading'>Global Crypto Stats</Title>
+     <Row>
+      <Col span={12}><Statistic title="Total Crptocurrencies" value='5'/></Col>
+      <Col span={12}><Statistic title="Total Exchange" value='5'/></Col>
+      <Col span={12}><Statistic title="Total Market Cap" value='5'/></Col>
+      <Col span={12}><Statistic title="Total 24th Volume" value='5'/></Col>
+      <Col span={12}><Statistic title="Total Markets" value='5'/></Col>
+     </Row>
+    </>
   )
 }
 
